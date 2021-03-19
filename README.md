00d8a88e0d5303e70aca1ae2252d6204cca22d9e
oolanlongoo <1024920598@qq.com>
2021-03-19 09:38:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dyson-game-tools
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dyson-game-tools)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: "", component: HomeComponent }])
+  ],
+  declarations: [AppComponent, TopBarComponent, HomeComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
